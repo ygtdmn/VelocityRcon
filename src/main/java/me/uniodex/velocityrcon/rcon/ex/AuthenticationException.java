042c555e5040721af28c899d678d212f76c5d4d3
Yiğit Duman <yigitq@gmail.com>
2019-08-19 20:43:19 +0300

    Removed unnecessary packages.

--- src/main/java/me/uniodex/velocityrcon/rcon/ex/AuthenticationException.java
+++
@@ -1,9 +0,0 @@
-package me.uniodex.velocityrcon.rcon.ex;
-
-public class AuthenticationException extends Exception {
-
-	public AuthenticationException(String message) {
-		super(message);
-	}
-	
-}
